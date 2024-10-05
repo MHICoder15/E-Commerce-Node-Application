9bc0798825ede3bcd010e1b1c0fe5903107136e8
Hasnain Malik <mhicoder@gmail.com>
2024-10-05 15:54:45 +0500

    Form Validation with error message functionality has been implemented.

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,10 +1,20 @@
 const Product = require("../models/product");
+const { validationResult } = require("express-validator");
 
 exports.getAddProduct = (req, res, next) => {
   res.render("admin/edit-product", {
     pageTitle: "Add Product",
     path: "/admin/add-product",
     editing: false,
+    hasError: false,
+    product: {
+      title: null,
+      imageUrl: null,
+      price: null,
+      description: null,
+    },
+    errorMessage: req.flash("error"),
+    validationErrors: [],
   });
 };
 
@@ -13,6 +23,25 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return res.status(422).render("admin/edit-product", {
+      pageTitle: "Add Product",
+      path: "/admin/add-product",
+      editing: false,
+      hasError: true,
+      product: {
+        title: title,
+        imageUrl: imageUrl,
+        price: price,
+        description: description,
+      },
+      errorMessage: errors.array()[0].msg,
+      validationErrors: errors.array().map((e) => e.path),
+    });
+  }
+
   const product = new Product({
     title: title,
     price: price,
@@ -46,7 +75,10 @@ exports.getEditProduct = (req, res, next) => {
         pageTitle: "Edit Product",
         path: "/admin/edit-product",
         editing: editMode,
+        hasError: true,
         product: product,
+        errorMessage: req.flash("error"),
+        validationErrors: [],
       });
     })
     .catch((err) => console.log("Find Product By ID Error:", err));
@@ -58,6 +90,25 @@ exports.postEditProduct = (req, res, next) => {
   const updatedPrice = req.body.price;
   const updatedImageUrl = req.body.imageUrl;
   const updatedDesc = req.body.description;
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return res.status(422).render("admin/edit-product", {
+      pageTitle: "Edit Product",
+      path: "/admin/edit-product",
+      editing: true,
+      hasError: true,
+      product: {
+        _id: prodId,
+        title: updatedTitle,
+        imageUrl: updatedImageUrl,
+        price: updatedPrice,
+        description: updatedDesc,
+      },
+      errorMessage: errors.array()[0].msg,
+      validationErrors: errors.array().map((e) => e.path),
+    });
+  }
 
   Product.findById(prodId)
     .then((product) => {
--- controllers/auth.js
+++ controllers/auth.js
@@ -3,6 +3,7 @@ const bcrypt = require("bcryptjs");
 const User = require("../models/user");
 const nodemailer = require("nodemailer");
 const dotenv = require("dotenv");
+const { validationResult } = require("express-validator");
 
 dotenv.config();
 const EMAIL_SERVICE = process.env.EMAIL_SERVICE;
@@ -21,7 +22,14 @@ exports.getRegister = (req, res, next) => {
   res.render("auth/register", {
     path: "/register",
     pageTitle: "Register",
+    inputData: {
+      name: null,
+      email: null,
+      password: null,
+      confirmPassword: null,
+    },
     errorMessage: req.flash("error"),
+    validationErrors: [],
   });
 };
 
@@ -30,107 +38,151 @@ exports.postRegister = (req, res, next) => {
   const email = req.body.email;
   const password = req.body.password;
   const confirmPassword = req.body.confirmPassword;
-
-  if (name && email && password && confirmPassword) {
-    if (password !== confirmPassword) {
-      req.flash("error", "Password does not matched.");
-      return res.redirect("/register");
-    }
-    User.findOne({ email: email })
-      .then((userData) => {
-        if (userData) {
-          req.flash("error", "Email is already existed.");
-          return res.redirect("/register");
-        }
-        bcrypt
-          .hash(password, 12)
-          .then((hashPassword) => {
-            const user = new User({
-              name: name,
-              email: email,
-              password: hashPassword,
-              cart: { items: [] },
-            });
-            user
-              .save()
-              .then((result) => {
-                const mailOptions = {
-                  from: EMAIL_USER,
-                  to: email,
-                  subject: "Signup succeeded.",
-                  html: "<h1>You have successfully signed up!</h1>",
-                };
-                transporter
-                  .sendMail(mailOptions)
-                  .then((info) => {
-                    console.log("Email sent: " + info.response);
-                    console.log("Created User");
-                    req.flash("success", "New user has been created.");
-                    res.redirect("/login");
-                  })
-                  .catch((error) => {
-                    console.log("Send Email to User Error:", error);
-                  });
-              })
-              .catch((err) => {
-                console.log("Add User Error:", err);
-              });
-          })
-          .catch((error) => {
-            console.log("Hashing Password Error", error);
-          });
-      })
-      .catch((error) => {
-        console.log("Find User Error", error);
-      });
-  } else {
-    req.flash("error", "Please fill-in valid data in all fields & try again.");
-    return res.redirect("/register");
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(422).render("auth/register", {
+      path: "/register",
+      pageTitle: "Register",
+      inputData: {
+        name: name,
+        email: email,
+        password: password,
+        confirmPassword: confirmPassword,
+      },
+      errorMessage: errors.array()[0].msg,
+      validationErrors: errors.array().map((e) => e.path),
+    });
   }
+  // if (password !== confirmPassword) {
+  //   req.flash("error", "Password does not matched.");
+  //   return res.redirect("/register");
+  // }
+  // User.findOne({ email: email })
+  //   .then((userData) => {
+  //     if (userData) {
+  //       req.flash(
+  //         "error",
+  //         "Email is already existed, please pick a different one."
+  //       );
+  //       return res.redirect("/register");
+  //     }
+  bcrypt
+    .hash(password, 12)
+    .then((hashPassword) => {
+      const user = new User({
+        name: name,
+        email: email,
+        password: hashPassword,
+        cart: { items: [] },
+      });
+      user
+        .save()
+        .then((result) => {
+          const mailOptions = {
+            from: EMAIL_USER,
+            to: email,
+            subject: "Signup succeeded.",
+            html: "<h1>You have successfully signed up!</h1>",
+          };
+          transporter
+            .sendMail(mailOptions)
+            .then((info) => {
+              console.log("Email sent: " + info.response);
+              console.log("Created User");
+              req.flash("success", "New user has been created.");
+              res.redirect("/login");
+            })
+            .catch((error) => {
+              console.log("Send Email to User Error:", error);
+            });
+        })
+        .catch((err) => {
+          console.log("Add User Error:", err);
+        });
+    })
+    .catch((error) => {
+      console.log("Hashing Password Error", error);
+    });
+  // })
+  // .catch((error) => {
+  //   console.log("Find User Error", error);
+  // });
 };
 
 exports.getLogin = (req, res, next) => {
   res.render("auth/login", {
     path: "/login",
     pageTitle: "Login",
+    inputData: {
+      email: null,
+      password: null,
+    },
     errorMessage: req.flash("error"),
     successMessage: req.flash("success"),
+    validationErrors: [],
   });
 };
 
 exports.postLogin = (req, res, next) => {
   const email = req.body.email;
   const password = req.body.password;
-  if (email && password) {
-    User.findOne({ email: email })
-      .then((user) => {
-        if (!user) {
-          req.flash("error", "Invalid email or password.");
-          return res.redirect("/login");
-        }
-        bcrypt
-          .compare(password, user.password)
-          .then((doMatch) => {
-            if (doMatch) {
-              req.session.isLoggedIn = true;
-              req.session.user = user;
-              return req.session.save((error) => {
-                if (error) console.log("Save Session Error:", error);
-                res.redirect("/");
-              });
-            }
-            req.flash("error", "Invalid email or password.");
-            res.redirect("/login");
-          })
-          .catch((error) => {
-            console.log("Comparing Password Error", error);
-          });
-      })
-      .catch((err) => console.log("Find User Error:", err));
-  } else {
-    req.flash("error", "Please fill-in valid data in all fields & try again.");
-    return res.redirect("/login");
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(422).render("auth/login", {
+      path: "/login",
+      pageTitle: "Login",
+      errorMessage: errors.array()[0].msg,
+      successMessage: req.flash("success"),
+      inputData: {
+        email: email,
+        password: password,
+      },
+      validationErrors: errors.array().map((e) => e.path),
+    });
   }
+  User.findOne({ email: email })
+    .then((user) => {
+      if (!user) {
+        req.flash("error", "Please enter a valid credentials & try again!");
+        return res.status(422).render("auth/login", {
+          path: "/login",
+          pageTitle: "Login",
+          errorMessage: req.flash("error"),
+          successMessage: req.flash("success"),
+          inputData: {
+            email: email,
+            password: password,
+          },
+        });
+      }
+      bcrypt
+        .compare(password, user.password)
+        .then((doMatch) => {
+          if (doMatch) {
+            req.session.isLoggedIn = true;
+            req.session.user = user;
+            return req.session.save((error) => {
+              if (error) console.log("Save Session Error:", error);
+              res.redirect("/");
+            });
+          }
+          req.flash("error", "Please enter a valid credentials & try again!");
+          return res.status(422).render("auth/login", {
+            path: "/login",
+            pageTitle: "Login",
+            errorMessage: req.flash("error"),
+            successMessage: req.flash("success"),
+            inputData: {
+              email: email,
+              password: password,
+            },
+          });
+        })
+        .catch((error) => {
+          console.log("Comparing Password Error", error);
+        });
+    })
+    .catch((err) => console.log("Find User Error:", err));
 };
 
 exports.postLogout = (req, res, next) => {
